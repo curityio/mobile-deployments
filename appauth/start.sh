4eb7046e3b9b826c53031ba48e4c6fa1b5238f61
gary.archer <gary.archer@WORKMAC.local>
2021-11-01 17:22:51 +0100

    Automation updates

--- appauth/start.sh
+++ appauth/start.sh
@@ -32,7 +32,7 @@ ngrok http 8443 -log=stdout &
 sleep 5
 export NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[] | select(.proto == "https") | .public_url')
 if [ "$NGROK_URL" == "" ]; then
-  echo "Problem encountered getting an NGROK URL"
+  echo 'Problem encountered getting an NGROK URL'
   exit 1
 fi
 
@@ -41,7 +41,7 @@ fi
 #
 docker compose --project-name appauth up --detach --force-recreate
 if [ $? -ne 0 ]; then
-  echo "Problem encountered starting Docker components"
+  echo 'Problem encountered starting Docker components'
   exit 1
 fi
 
